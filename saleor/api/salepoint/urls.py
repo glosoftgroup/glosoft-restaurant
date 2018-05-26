43c2d5ef8fb85b95c230c70f9250df5fc8ab4023
Kiburu Alex <alexkiburu@gmail.com>
2018-05-26 12:43:32 +0300

    1. added counters and kitchens url, serializer, and viewset for frontend points, 2. seperation of concerns in the ordered items serializer

--- saleor/api/salepoint/urls.py
+++ saleor/api/salepoint/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 
 from .views import (
-    SalePointListAPIView
+    SalePointListAPIView,
+    CountersAndKitchensListAPIView
     )
 
 
 urlpatterns = [
     url(r'^$', SalePointListAPIView.as_view(), name='api-sale_point-list'),
+    url(r'^points/$', CountersAndKitchensListAPIView.as_view(), name='api-counters-kitchens-list'),
 ]
 
--- saleor/api/salepoint/views.py
+++ saleor/api/salepoint/views.py
@@ -1,12 +1,35 @@
 from ...salepoints.models import SalePoint
+from saleor.counter.models import Counter
+from saleor.kitchen.models import Kitchen
 from .serializers import (
     SalePointListSerializer,
+    CountersAndKitchensSerializer,
      )
-from rest_framework import generics
+from rest_framework import generics, views
+from rest_framework.response import Response
 from django.contrib.auth import get_user_model
-User = get_user_model()
 
+User = get_user_model()
 
 class SalePointListAPIView(generics.ListAPIView):
     serializer_class = SalePointListSerializer
     queryset = SalePoint.objects.all()
+
+class CustomSalePointListAPIView(generics.ListAPIView):
+    serializer_class = SalePointListSerializer
+    queryset = SalePoint.objects.all()
+
+class CountersAndKitchensListAPIView(views.APIView):
+
+    def get(self, request):
+    	points = []
+    	for counter in Counter.objects.all():
+    		setattr(counter, "point", "counter")
+    		points.append(counter)
+
+    	for kitchen in Kitchen.objects.all():
+    		setattr(kitchen, "point", "kitchen")
+    		points.append(kitchen)
+
+        results = CountersAndKitchensSerializer(points, many=True).data
+        return Response(results)
