adcd37bd80e8f090791ade2f9edf8d8f5cd6fb11
Kiburu Alex <alex.kiburu@glosoftgroup.com>
2018-06-05 17:24:20 +0300

    1. Changes the api/category/view to use serializer conditionality based on the name of the section 2. added an additional menucategoryserializer in the api/category/serializer

--- saleor/api/category/views.py
+++ saleor/api/category/views.py
@@ -5,8 +5,10 @@ from django.contrib.auth import get_user_model
 
 from .serializers import (
     CategoryListSerializer,
+    MenuCategoryListSerializer
      )
 from ...product.models import Category
+from saleor.menucategory.models import MenuCategory
 
 User = get_user_model()
 
@@ -23,8 +25,18 @@ class BusinessTypeCategoryListAPIView(generics.ListAPIView):
 
     payload: /payload/businesstype-categories.json
     """
-    serializer_class = CategoryListSerializer
-    queryset = Category.objects.all()
+
+    """ choose the default serializer class based on the name """
+    def get_serializer_class(self):
+        if self.kwargs["name"].lower() == "restaurant": # here add the logic to decide the asset type
+             return MenuCategoryListSerializer
+        return CategoryListSerializer
+
+    """ choose the default queryset based on the name """
+    def get_queryset(self):
+        if self.kwargs["name"].lower() == "restaurant":  # here add the logic to decide the asset type
+            return MenuCategory.objects.all()
+        return Category.objects.all()
 
     def list(self, request, pk=None, name=None):
         serializer_context = {
@@ -32,9 +44,13 @@ class BusinessTypeCategoryListAPIView(generics.ListAPIView):
         }
         if pk:
             queryset = self.get_queryset().filter(section__name=name)
+            serializer = CategoryListSerializer(queryset, context=serializer_context, many=True)
         if name:
-            queryset = self.get_queryset().filter(section__name=name)
+            if name.lower() == "bar":
+                queryset = self.get_queryset().filter(section__name__icontains=name)
+                serializer = CategoryListSerializer(queryset, context=serializer_context, many=True)
+            elif name.lower() == "restaurant":
+                serializer = MenuCategoryListSerializer(self.get_queryset(), context=serializer_context, many=True)
 
-        serializer = CategoryListSerializer(queryset, context=serializer_context, many=True)
         return Response(serializer.data)
 
